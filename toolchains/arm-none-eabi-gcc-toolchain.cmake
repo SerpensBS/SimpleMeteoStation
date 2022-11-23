6af91854fb0da4079f1deafc8ba1795c2214f8fa
Laputin Evgenii <serpensbs@gmail.com>
2022-11-23 16:03:08 +0300

    Пофиксил и дополнил Readme.md, а также добавил поиск утилит для сборки по данным из параметров cmake.

--- toolchains/arm-none-eabi-gcc-toolchain.cmake
+++ toolchains/arm-none-eabi-gcc-toolchain.cmake
@@ -1,8 +1,12 @@
-set(TOOLCHAIN_PREFIX arm-none-eabi-)
+if ("${CMAKE_C_COMPILER}" STREQUAL "" OR "${CMAKE_CXX_COMPILER}" STREQUAL "" )
+    return()
+endif()
 
-set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}gcc)
+# За директорию, в которой лежат остальные файлы toolchain'а, примем директорию, в которой лежит компилятор C++.
+get_filename_component(TOOLCHAIN_DIRECTORY ${CMAKE_CXX_COMPILER} DIRECTORY)
+set(TOOLCHAIN_PREFIX ${TOOLCHAIN_DIRECTORY}/arm-none-eabi-)
+
 set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
-set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}g++)
 set(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}objcopy)
 set(CMAKE_OBJDUMP ${TOOLCHAIN_PREFIX}objdump)
 set(CMAKE_SIZE ${TOOLCHAIN_PREFIX}size)
